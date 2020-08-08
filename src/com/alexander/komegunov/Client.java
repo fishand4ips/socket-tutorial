f05a37fa97475d1c390fc18f5cec105203704b4d
fishand4ips <alekomegunov@gmail.com>
2020-08-08 14:50:08 +0300

    initial commit

---
+++ src/com/alexander/komegunov/Client.java
@@ -0,0 +1,59 @@
+package com.alexander.komegunov;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        final String serverHost = "localhost";
+        Socket socketOfClient;
+        BufferedWriter os;
+        BufferedReader is;
+
+        try {
+            socketOfClient = new Socket(serverHost, 7777);
+            os = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
+            is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
+
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + serverHost);
+            return;
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + serverHost);
+            return;
+        }
+
+        try {
+            os.write("HELO! now is " + new Date());
+            os.newLine();
+            os.flush();
+            os.write("I am Tom Cat");
+            os.newLine();
+            os.flush();
+            os.write("QUIT");
+            os.newLine();
+            os.flush();
+
+            String responseLine;
+            while ((responseLine = is.readLine()) != null) {
+                System.out.println("Server: " + responseLine);
+                if (responseLine.contains("OK")) {
+                    break;
+                }
+            }
+
+            os.close();
+            is.close();
+            socketOfClient.close();
+        } catch (UnknownHostException e) {
+            System.err.println("Trying to connect to unknown host: " + e);
+        } catch (IOException e) {
+            System.err.println("IOException:  " + e);
+        }
+    }
+
+}
